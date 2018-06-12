45d25ae6eb27e00a5ae36d46fd7cc7efa1fe4e28
JonathanMasias <jonathanmasiasflores@gmail.com>
2018-06-11 23:12:51 -0500

    Subiendo carpeta - wallpapers
    Esto es para fontos de dispositivos

---
+++ public_html/img/carousel-2/js/slider.js
@@ -0,0 +1,48 @@
+//almacenar slider en una variable
+var slider = $('#slider');
+//almacenar botones
+var siguiente = $('#btn-next');
+var anterior = $('#btn-prev');
+
+//mover ultima imagen al primer lugar
+$('#slider section:last').insertBefore('#slider section:first');
+//mostrar la primera imagen con un margen de -100%
+slider.css('margin-left', '-'+100+'%');
+
+function moverD() {
+	slider.animate({
+		marginLeft:'-'+200+'%'
+	} ,700, function(){
+		$('#slider section:first').insertAfter('#slider section:last');
+		slider.css('margin-left', '-'+100+'%');
+	});
+}
+
+function moverI() {
+	slider.animate({
+		marginLeft:0
+	} ,700, function(){
+		$('#slider section:last').insertBefore('#slider section:first');
+		slider.css('margin-left', '-'+100+'%');
+	});
+}
+
+siguiente.on('click',function() {
+	moverD();
+	clearInterval(interval);
+	autoplay();
+});
+
+anterior.on('click',function() {
+	moverI();
+	clearInterval(interval);
+	autoplay();
+});
+
+function autoplay() {
+	interval = setInterval(function(){
+		moverD();
+	}, 8000);
+}
+
+autoplay();
---
+++ public_html/js/cite-bible.js
@@ -0,0 +1,29 @@
+
+	var refTagger = {
+		settings: {
+			bibleVersion: "RVR60",
+			dropShadow: false, 
+			roundCorners: true,
+			socialSharing: ["twitter","facebook"],
+			customStyle : {
+				heading: {
+					backgroundColor : "#073763",
+					color : "#ffffff",
+					fontFamily : "Palatino, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif"
+				},
+				body   : {
+					color : "#0000ff",
+					fontFamily : "TimesNewRoman, 'Times New Roman', Times, Baskerville, Georgia, serif",
+					fontSize : "18px",
+					moreLink : {
+						color: "#d9d9d9"
+					}
+				}
+			}
+		}
+	};
+	(function(d, t) {
+		var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+		g.src = "//api.reftagger.com/v2/RefTagger.es.js";
+		s.parentNode.insertBefore(g, s);
+	}(document, "script"));
